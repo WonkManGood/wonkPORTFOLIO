4181c182f517e6ea9864332aba6c63cc3cdfd9b4
WonkManGood <ezrajwadding@gmail.com>
2025-01-04 19:34:41 +0000

    01.04

--- app.py
+++ app.py
@@ -1,11 +1,11 @@
 from flask import Flask, render_template
 app = Flask(__name__, static_folder='assets')
 
+def test():
+    print('Hello World')
+
 @app.route('/')
 def main():
     return render_template('base.html')
 
-while True:
-    app.run(host='0.0.0.0', port='80')
-    if KeyboardInterrupt:
-        break
+app.run(host='0.0.0.0', port='2000')
