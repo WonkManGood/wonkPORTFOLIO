2769d588ccd516a05c6b15dd80054badaa602cfa
WonkManGood <ezrajwadding@gmail.com>
2025-01-07 01:07:29 +0000

    01.06

--- app.py
+++ app.py
@@ -1,13 +1,11 @@
 from flask import Flask, render_template
-from werkzeug.middleware.proxy_fix import ProxyFix
 app = Flask(__name__, static_folder='assets')
-app.wsgi_app = ProxyFix(app.wsgi_app)
 
 def test():
     print('Hello World')
 
 @app.route('/')
 def main():
-    return render_template('base.html')
+    return render_template('base.html') # 
 
-app.run(host='0.0.0.0', port='1024')
+app.run(host='0.0.0.0', port='1024', debug=False)
